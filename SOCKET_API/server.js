2a34d9f02cc159025cc3425fb04be3dcee22b90d
antonioramana <antonioramanandraibe@gmail.com>
2024-07-15 17:52:25 +0200

    bugfix: change socket API code

--- SOCKET_API/server.js
+++ SOCKET_API/server.js
@@ -1,22 +1,74 @@
+// const express = require('express');
+// const http = require('http');
+// const socketIo = require('socket.io');
+// const cors = require('cors');
+
+// const app = express();
+// const server = http.createServer(app);
+
+// app.use(cors({
+//     origin: '*', 
+//     methods: ["GET", "POST"]
+// }));
+
+// const io = socketIo(server, {
+//     cors: {
+//         origin: "*",
+//         methods: ["GET", "POST"]
+//     }
+// });
+// let users = [];
+// let interviewActive = false;
+// let isExpiredInterview = false;
+
+// io.on('connection', (socket) => {
+//     io.emit('updateUsers', users);
+//     console.log('A user connected');
+
+//     socket.on('activateInterview', () => {
+//        // interviewActive = true;
+//         io.emit("interviewActive");
+        
+//     });
+
+//     socket.on('startInterview', (userData) => {
+//         users.push(userData);
+//         io.emit('updateUsers', users);
+//     });
+
+//     socket.on('interviewExpired', () => {
+//         //users = users.filter(user => user.socketId !== socketId);
+//         //io.emit('updateUsers', users);
+//         //io.to(socketId).emit('interviewExpired');
+//         isExpiredInterview = true;
+//         io.emit("interviewExpired");
+//     });
+
+//     socket.on('disconnect', () => {
+//         console.log('User disconnected');
+//     });
+// });
+
+// server.listen(4000, () => {
+//     console.log('Server is running on port 4000');
+// });
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const { Server } = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-const server = http.createServer(app);
+const io = new Server(4000, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
 
 app.use(cors({
     origin: '*', 
-    methods: ["GET", "POST"]
+    methods: ['GET', 'POST']
 }));
 
-const io = socketIo(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
-});
 let users = [];
 let interviewActive = false;
 let isExpiredInterview = false;
@@ -26,9 +78,8 @@ io.on('connection', (socket) => {
     console.log('A user connected');
 
     socket.on('activateInterview', () => {
-       // interviewActive = true;
-        io.emit("interviewActive");
-        
+        interviewActive = true;
+        io.emit('interviewActive');
     });
 
     socket.on('startInterview', (userData) => {
@@ -37,11 +88,8 @@ io.on('connection', (socket) => {
     });
 
     socket.on('interviewExpired', () => {
-        //users = users.filter(user => user.socketId !== socketId);
-        //io.emit('updateUsers', users);
-        //io.to(socketId).emit('interviewExpired');
         isExpiredInterview = true;
-        io.emit("interviewExpired");
+        io.emit('interviewExpired');
     });
 
     socket.on('disconnect', () => {
@@ -49,6 +97,5 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(4000, () => {
-    console.log('Server is running on port 4000');
-});
+console.log('Server is running on port 4000');
+
